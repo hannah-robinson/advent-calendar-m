bb5913c835fcec8852b0602f4c97c9733015481d
Hannah Robinson <hannah.robinson.git@gmail.com>
2020-11-30 20:10:02 +1300

    Add JS for creating doors: number fades out and URL background fades in

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const calendarContainer = document.querySelector(".container")
 
 const calendarDays = 25;
 
+const openDoor = (path, event) => {
+  event.target.parentNode.style.backgroundImage = `url(${path})`;
+  event.target.style.opacity = "0";
+  event.target.style.backgroundColor = "#503E1F";
+}
+
 const createCalendar = () => {
   for(let i = 0; i < calendarDays; i++) {
     const calendarDoor = document.createElement("div");
@@ -12,9 +18,14 @@ const createCalendar = () => {
     calendarDoor.style.gridArea = "door" + (i + 1);
     calendarContainer.appendChild(calendarDoor);
 
-    calendarDoorText.classList.add("text");
+    calendarDoorText.classList.add("number");
     calendarDoorText.innerHTML = i + 1;
     calendarDoor.appendChild(calendarDoorText);
+
+    dayNumber = i + 1;
+    let dayImagePath = `./images/advent-m-${dayNumber}.jpg`;
+
+    calendarDoorText.addEventListener("click", openDoor.bind(null, dayImagePath));
   }
 }
 
